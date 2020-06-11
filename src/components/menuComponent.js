d3810c2c0f9ee3ca7a958c8e43f8e1447ec1982c
Devansh Kumar <kumardevansh8@gmail.com>
2020-06-12 01:16:12 +0530

    Lifecycle Part-I

--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -9,8 +9,12 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
     }
 
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked ');
+    }
     onDishSelect(dish) {
         this.setState({ selectedDish: dish});
     }
@@ -46,6 +50,8 @@ class Menu extends Component {
                 </div>
             );
         });
+        console.log('Menu Component render is invoked');
+
 
         return (
             <div className="container">
