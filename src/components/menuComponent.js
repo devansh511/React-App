267db884c080f887803de07c2093e6b01e782c5f
Devansh Kumar <kumardevansh8@gmail.com>
2020-06-18 01:26:02 +0530

    Header and Footer

--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -14,26 +14,6 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
             </Card>
         );
     }
-    
-
-    /* renderDish(dish) {
-        if (dish != null ) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />                        
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else{
-            return (
-                <div></div>
-            );
-        }
-    }*/
     const Menu = (props) => {
         const menu = props.dishes.map((dish) => {
             return (
